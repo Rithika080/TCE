dec13fa5087a7ca54d77e2dfda8379a3198410e1
Rithika080 <Shettyrithikar080@gmail.com>
2023-10-15 19:52:37 +0530

    week 1

---
+++ day3/index.js
@@ -0,0 +1,59 @@
+// prompt('Enter your Name',"");
+// console.log("this is console");
+// console.log(6+5);
+// document.write("<h1> Javasript </h1>");
+
+
+// var num=5
+// var num =7//we can redeclare the variable
+// num=4//we can reassign the variable
+// console.log(num);
+
+// let num1=6
+// //let num1=7 //we can't redeclare the variable
+// num1=5 //we can reassign the variable
+// console.log(num1);
+
+// const num2=6
+// //const num =7//we can't redeclare the variable
+// //num=4//we can't reassign the variable
+// console.log(num2);
+
+//Assignment operaters
+// console.log(3**4);
+// let x=5
+// x++
+
+// let num=5
+// let num1="5"
+// console.log(num === num1);
+
+
+// Functions
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+// }
+// document.write(mul(4,5,6,7));
+//  function greet(){
+//     alert("hello everyone")
+//  }
+
+// let a=10
+// if(a>18){
+//     document.write("eligible")
+// }
+// else{
+//     document.write("not eligible")
+// }
+
+// for(n=10 ; n>=1;n--){
+//     console.log(n);
+// }
+
+
+//DOM manipulatio:
+
+// let el=document.getElementById("head").innerHTML
+// console.log(el);
+let el=document.querySelector("#head").innerHTML
+console.log(el);
